1c5ff831ca4dde4a3f68eb385d6e0b65076feabf
Andrew Orians <oriansaj@rose-hulman.edu>
2020-11-01 17:03:53 -0500

    Initial JS

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,27 +3,59 @@
  * Provides the JavaScript interactions for all pages.
  *
  * @author 
- * Andrew Orians
+ * Andrew Orians, Sam Dunaway
  */
 
 /** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.FB_COLLECTION_USERS = "Users";
+rhit.FB_COLLECTION_ROUTES = "Routes";
+rhit.fbAuthManager = null;
+rhit.fbRoutesManager = null;
+rhit.fbSingleRouteManager = null;
+rhit.fbStatsManager = null;
 
-/** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
+function htmlToElement(html) {
+	var template = document.createElement("template");
+	html = html.trim();
+	template.innerHTML = html;
+	return template.content.firstChild;
+}
 
-rhit.ClassName = class {
+rhit.FbAuthManager = class {
 	constructor() {
+		this._user = null;
+	}
+	beginListening(changeListener) {
+		firebase.auth().onAuthStateChanged((user) => {
+			this._user = user;
+			changeListener();
+		});
+	}
+	signOut() {
+		firebase.auth().signOut().then(function () {
+			console.log("You are now signed out");
+		}).catch(function (error) {
+			// An error happened.
+			console.log("Sign out error");
+		});
+	}
+	get isSignedIn() {
+		return !!this._user;
+	}
+	get uid() {
+		return this._user.uid;
+	}
+}
 
+rhit.checkForRedirects = function () {
+	if (document.querySelector("#loginPage") && rhit.fbAuthManager.isSignedIn) {
+		window.location.href = `/list.html?uid=${rhit.fbAuthManager.uid}`;
 	}
 
-	methodName() {
-
+	if (!document.querySelector("#loginPage") && !rhit.fbAuthManager.isSignedIn) {
+		window.location.href = "/";
 	}
 }
 
