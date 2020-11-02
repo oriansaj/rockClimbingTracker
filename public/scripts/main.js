ce689ba48a388301b6222bf814f95c4535afed38
Andrew Orians <oriansaj@rose-hulman.edu>
2020-11-02 00:04:46 -0500

    Public list page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -15,6 +15,10 @@ rhit.FB_KEY_ROUTES = "routes";
 rhit.FB_KEY_NOTES = "notes";
 rhit.FB_KEY_START_DATES = "startDates";
 rhit.FB_KEY_IN_PROGRESS = "inProgress";
+rhit.FB_KEY_NAME = "name";
+rhit.FB_KEY_DIFFICULTY = "difficulty";
+rhit.FB_KEY_LOCATION = "location";
+rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
 rhit.fbAuthManager = null;
 rhit.fbRoutesManager = null;
 rhit.fbSingleRouteManager = null;
@@ -60,9 +64,9 @@ rhit.FbAuthManager = class {
 		});
 	}
 	signOut() {
-		firebase.auth().signOut().then(function () {
+		firebase.auth().signOut().then(() => {
 			console.log("You are now signed out");
-		}).catch(function (error) {
+		}).catch((error) => {
 			// An error happened.
 			console.log("Sign out error");
 		});
@@ -72,6 +76,119 @@ rhit.FbAuthManager = class {
 	}
 	get uid() {
 		return this._user.uid;
+	}
+}
+
+rhit.ListPageController = class {
+	constructor() {
+		document.querySelector("#toAllRoutes").addEventListener("click", (event) => {
+			window.location.href = "/list.html";
+		});
+		document.querySelector("#toMyRoutes").addEventListener("click", (event) => {
+			window.location.href = `/list.html?uid=${rhit.fbAuthManager.uid}`;
+		});
+		document.querySelector("#toMyStats").addEventListener("click", (event) => {
+			window.location.href = `/stats.html?uid=${rhit.fbAuthManager.uid}`;
+		});
+		document.querySelector("#menuSignOut").addEventListener("click", (event) => {
+			rhit.fbAuthManager.signOut();
+		});
+
+		// document.querySelector("#submitAddPhoto").addEventListener("click", (event) => {
+		// 	const url = document.querySelector("#inputUrl").value;
+		// 	const caption = document.querySelector("#inputCaption").value;
+		// 	rhit.fbPhotoBucketManager.add(url, caption);
+		// });
+
+		// $("#addPhotoDialog").on("show.bs.modal", (event) => {
+		// 	document.querySelector("#inputUrl").value = "";
+		// 	document.querySelector("#inputCaption").value = "";
+		// });
+		// $("#addPhotoDialog").on("shown.bs.modal", (event) => {
+		// 	document.querySelector("#inputUrl").focus();
+		// });
+
+		rhit.fbRoutesManager.beginListening(this.updateList.bind(this));
+	}
+
+	_createCard(route) {
+		return htmlToElement(`<div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${route.name}</h5>
+          <img src="${route.img}" alt="" class="inProgressDot">
+        </div>
+      </div>`);
+	}
+
+	updateList() {
+		const newList = htmlToElement('<div id="routeListContainer"></div>');
+
+		for (let i = 0; i < rhit.fbRoutesManager.length; i++) {
+			const route = rhit.fbRoutesManager.getRouteAtIndex(i);
+			const newCard = this._createCard(route);
+			newCard.onclick = (event) => {
+				window.location.href = `/routeview.html?id=${route.id}`;
+			};
+			newList.appendChild(newCard);
+		}
+
+		const oldList = document.querySelector("#routeListContainer");
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+
+		oldList.parentElement.appendChild(newList);
+	}
+}
+
+rhit.Route = class {
+	constructor(id, name, img) {
+		this.id = id;
+		this.name = name;
+		this.img = img;
+	}
+}
+
+rhit.FbRoutesManager = class {
+	constructor(uid) {
+		this._uid = uid;
+		this._documentSnapshots = [];
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_ROUTES);
+		this._unsubscribe = null;
+	}
+	add(name, difficulty, location) {
+		this._ref.add({
+				[rhit.FB_KEY_NAME]: name,
+				[rhit.FB_KEY_DIFFICULTY]: difficulty,
+				[rhit.FB_KEY_LOCATION]: location,
+				[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now()
+			})
+			.then(function (docRef) {
+				console.log("Document written with ID: ", docRef.id);
+			})
+			.catch(function (error) {
+				console.error("Error adding document: ", error);
+			});
+	}
+	beginListening(changeListener) {
+		let query = this._ref.orderBy(rhit.FB_KEY_LAST_TOUCHED, "desc").limit(50);
+		// if (this._uid) {
+		// 	query = query.where(rhit.FB_KEY_AUTHOR, "==", this._uid);
+		// }
+		query.onSnapshot((querySnapshot) => {
+			this._documentSnapshots = querySnapshot.docs;
+			changeListener();
+		});
+	}
+	stopListening() {
+		this._unsubscribe();
+	}
+	get length() {
+		return this._documentSnapshots.length;
+	}
+	getRouteAtIndex(index) {
+		const docSnapshot = this._documentSnapshots[index];
+		const route = new rhit.Route(docSnapshot.id, docSnapshot.get(rhit.FB_KEY_NAME), docSnapshot.get(rhit.FB_KEY_DIFFICULTY), docSnapshot.get(rhit.FB_KEY_LOCATION));
+		return route;
 	}
 }
 
@@ -90,13 +207,13 @@ rhit.initializePage = function () {
 		rhit.startFirebaseUI();
 	}
 
-	// if (document.querySelector("#listPage")) {
-	// 	const urlParams = new URLSearchParams(window.location.search);
-	// 	const userId = urlParams.get("uid");
+	if (document.querySelector("#listPage")) {
+		const urlParams = new URLSearchParams(window.location.search);
+		const userId = urlParams.get("uid");
 
-	// 	rhit.fbPhotoBucketManager = new rhit.FbPhotoBucketManager(userId);
-	// 	new rhit.ListPageController();
-	// }
+		rhit.fbRoutesManager = new rhit.FbRoutesManager(userId);
+		new rhit.ListPageController();
+	}
 
 	// if (document.querySelector("#detailPage")) {
 	// 	const urlParams = new URLSearchParams(window.location.search);
