c4295c72bacc1c170f0a80cef657812f501c63fb
Andrew Orians <oriansaj@rose-hulman.edu>
2020-11-08 19:54:50 -0500

    Added map template

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -287,6 +287,8 @@ rhit.DetailPageController = class {
 			rhit.fbAuthManager.signOut();
 		});
 
+		initMap();
+
 		rhit.fbSingleRouteManager.beginListening(this.updateView.bind(this));
 	}
 
@@ -313,6 +315,24 @@ rhit.DetailPageController = class {
 		// 	document.querySelector("#menuDelete").style.display = "flex";
 		// }
 	}
+}
+
+function initMap() {
+	// The location of Uluru
+	const uluru = {
+		lat: -25.344,
+		lng: 131.036
+	};
+	// The map, centered at Uluru
+	const map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 4,
+		center: uluru,
+	});
+	// The marker, positioned at Uluru
+	const marker = new google.maps.Marker({
+		position: uluru,
+		map: map,
+	});
 }
 
 rhit.FbSingleRouteManager = class {
